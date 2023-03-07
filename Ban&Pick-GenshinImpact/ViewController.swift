8a2e352d181c58c123513e644b1b63e98aed7667
qq1823988089 <1823988089@qq.com>
2023-03-07 15:48:20 +0800

    Initial Commit

---
+++ Ban&Pick-GenshinImpact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ban&Pick-GenshinImpact
+//
+//  Created by 乔一 on 2023/3/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
